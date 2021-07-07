cd4cb5f0eba4abff3b371618ce8b9046e16b2191
Abinath07 <abijohn1894@gmailcom>
2021-07-07 13:06:48 +0530

    updated

---
+++ src/org/test/Employee.java
@@ -0,0 +1,11 @@
+package org.test;
+
+public class Employee {
+
+	public static void employeeName() {
+		System.out.println("employee name is hi");
+	}
+	public static void main(String[] args) {
+		employeeName();
+	}
+}
