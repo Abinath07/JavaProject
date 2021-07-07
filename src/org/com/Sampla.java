cf833caa40d3e38eec4b2133eb3963e2c07f0cb7
Abinath07 <abijohn1894@gmailcom>
2021-07-07 14:13:14 +0530

    project updated

--- src/org/com/Sampla.java
+++ src/org/com/Sampla.java
@@ -1,5 +1,11 @@
 package org.com;
 
 public class Sampla {
-
+	
+	public static void employee() {
+		System.out.println("employee name");
+	}
+	public static void main(String[] args) {
+		employee();
+	}
 }
