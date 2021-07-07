32c19bc733e70451a0741ecb2a19ea056e75a839
Abinath07 <abijohn1894@gmailcom>
2021-07-07 13:11:53 +0530

    latest

---
+++ src/org/test/Employee.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class Employee {
+	
+	public static void employee() {
+		System.out.println("employee id is 123");
+	}
+	public static void main(String[] args) {
+		employee();
+	}
+
+}
